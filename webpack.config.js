79282e90dbb9057aff64a46dc1240ba9e7a330c4
Marc Heimgartner <marc.heimgartner@gmx.ch>
2017-02-07 10:39:02 +0100

    Updated all dependencies (webpack 2) and enabled hot model reloading

--- webpack.config.js
+++ webpack.config.js
@@ -46,10 +46,11 @@ module.exports = {
     extensions: [".webpack.js", ".web.js", ".js", ".vue"]
   },
   devServer: {
-    hot: true,
+    noInfo: true,
+    clientLogLevel: 'warning',
     proxy: {
       '/api/**': {
-        target: 'http://127.0.0.1:8080',
+	      target: 'http://127.0.0.1:8080',
         secure: false,
         changeOrigin: true,
         pathRewrite: {'^/api': ''}
