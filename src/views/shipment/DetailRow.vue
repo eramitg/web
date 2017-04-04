9afacd339a0a582a317e9528f2c022e5e5c81fbf
Marc Heimgartner <marc.heimgartner@gmx.ch>
2017-04-04 12:09:37 +0200

    Better padding for Graphs

--- src/views/shipment/DetailRow.vue
+++ src/views/shipment/DetailRow.vue
@@ -126,6 +126,13 @@ export default {
     },
     layout () {
       return {
+        margin: {
+          l: 50,
+          r: 50,
+          b: 50,
+          t: 50,
+          pad: 4
+        },
         yaxis: {
           title: '°C',
           range: this.range
